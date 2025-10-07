43d851a69934ef2c8969b34d54e8ea1db4a11b44
Professor Breaker <jbreak1@ju.edu>
2025-10-07 13:44:10 -0400

    UITableView’s Data Source

---
+++ LootLoggerRB/ItemStore.swift
@@ -0,0 +1,29 @@
+//
+//  ItemStore.swift
+//  LootLoggerRB
+//
+//  Created by P.Breaker on 10/7/25.
+//
+
+import UIKit
+
+class ItemStore {
+
+    var allItems = [Item]()
+    
+    //Adding an item creation method
+    @discardableResult func createItem() -> Item {
+        let newItem = Item(random: true)
+
+        allItems.append(newItem)
+
+        return newItem
+    }
+    
+    init() {
+        for _ in 0..<5 {
+            createItem()
+        }
+    }
+
+}
